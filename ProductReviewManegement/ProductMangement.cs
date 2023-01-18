2a6ea3f62b302ac2b24f4229d7e3bd1cf2871943
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-18 13:31:38 +0530

    [Kiran] Add: Adding Vlaues

---
+++ ProductReviewManegement/ProductMangement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManegement
+{
+    public class ProductMangement
+    {
+        List<ProductReview> productReviewList = new List<ProductReview>();
+
+        public void AddProductList(List<ProductReview> list)
+        {
+            this.productReviewList = list;
+        }
+    }
+}
